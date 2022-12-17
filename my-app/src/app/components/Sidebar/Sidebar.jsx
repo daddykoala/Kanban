b2bed6ec8ac38763af69a76a8ee37baefb8e7a45
clément moretti <98534268+daddykoala@users.noreply.github.com>
2022-12-17 21:05:53 +0100

    redux en cours

--- my-app/src/app/components/Sidebar/Sidebar.jsx
+++ my-app/src/app/components/Sidebar/Sidebar.jsx
@@ -1,16 +1,19 @@
 //import PropTypes from 'prop-types';
 import React from 'react';
-
+import { useSelector } from 'react-redux';
 
 import './sidebarStyles.scss';
 
-function sidebar() {
+function Sidebar() {
+    const tableauName = useSelector(state => state.tableauName)
+    console.log(tableauName,);
 return (
     <div>
         <section className='sidebar'>
         <h2>tableaux</h2>
         <div>
         <h2>mes tableaux</h2>
+        <span>{tableauName}</span>
         
 
         </div>
@@ -22,4 +25,4 @@ return (
 
 //sidebar.defaultProps = {};
 
-export default React.memo(sidebar);
+export default React.memo(Sidebar);
---
+++ my-app/src/reducer/tableauRducer.jsx
@@ -0,0 +1,10 @@
+const INITIALSTATE ={
+    
+        tableauName:"planning familial",
+}
+
+function TableauReducer(state = INITIALSTATE, action){
+    return state
+}
+
+export default TableauReducer;
