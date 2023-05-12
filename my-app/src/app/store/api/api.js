8ddd01ed9d779a18bff4160dc55134f44816d668
clément moretti <98534268+daddykoala@users.noreply.github.com>
2023-05-12 11:27:56 +0200

    creation hooks addtable

--- my-app/src/app/store/api/api.js
+++ my-app/src/app/store/api/api.js
@@ -1,18 +1,27 @@
 // Need to use the React-specific entry point to import createApi
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 // Define a service using a base URL and expected endpoints
 export const userApi = createApi({
-  reducerPath: 'userApi',
-  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3002' }),
+  reducerPath: "userApi",
+  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:3002" }),
   endpoints: (builder) => ({
-
     getContentByUser: builder.query({
       query: (id) => `users/${id}`,
     }),
+    //je crée un tableau pour un user
+    addTableByUser: builder.mutation({
+      //j'envoi un objet json avec l'id du user et le nom du tableau
+      query: (body) => ({
+        url: `tables`,
+        method: "POST",
+        body: { name: body.name, user_id: body.userId },
+      }),
+    }),
   }),
-})
+});
 
 // Export hooks for usage in functional components, which are
 // auto-generated based on the defined endpoints
-export const { useGetContentByUserQuery } = userApi
+//j'exporte les hooks pour les utiliser dans les composants fonctionnels
+export const { useGetContentByUserQuery, useAddTableByUserMutation } = userApi;
