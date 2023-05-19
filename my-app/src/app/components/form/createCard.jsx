5c5049ef791480ce5e7fa75b422c004b77fe664f
clément moretti <98534268+daddykoala@users.noreply.github.com>
2023-05-19 17:25:01 +0200

    css button add list

--- my-app/src/app/components/form/createCard.jsx
+++ my-app/src/app/components/form/createCard.jsx
@@ -3,9 +3,11 @@ import { useDispatch, useSelector } from "react-redux";
 import { usePostListByUserMutation } from "../../store/api/api";
 import { addList } from "../../store/reducer/userSlice";
 import{IoMdAddCircleOutline} from "react-icons/io"
+import {IoSend} from "react-icons/io5"
 
 function CreateCard({ tableId }) {
   const dispatch = useDispatch();
+  const [Editing, setEditing] = useState(false);
 
   const [postListbyUser, { data, isSuccess }] = usePostListByUserMutation();
     const [inputValue, setInputValue] = useState("");
@@ -31,21 +33,42 @@ function CreateCard({ tableId }) {
 
     }
   }
-
+  function opensForm() {
+    setEditing(true);
+  }
+  function closeForm() {
+    setEditing(false);
+  }
   function handleInputChange(e) {
     setInputValue(e.target.value);
   }
 
   return (
-    <div >
-      <form className="create__card"onSubmit={handleSubmit}>
-        <input type="text" value={inputValue} onChange={handleInputChange} />
-        <h3>ajout d'une liste</h3>
-        <button className="create__card__button"><IoMdAddCircleOutline/></button>
-      </form>
+    <div>
+      {Editing ? (
+        <div >
+        <form className="create__card" onSubmit={handleSubmit}>
+        <button onClick={opensForm}>
+        <IoMdAddCircleOutline className="create__card__button"/>
+      </button>
+      <div className="input__anim">
+
+            <input className="create__card__input maj"type="text" value={inputValue} onChange={handleInputChange} />
+          
+             <button className="create__card__button__input maj" onClick={closeForm}><IoSend/></button>
+      </div>
+        </form>
+    
+        </div>
+      ) : (
+        <button onClick={opensForm}>
+          <IoMdAddCircleOutline className="create__card__button"/>
+        </button>
+      )}
     </div>
   );
 }
+
 // createCard.propTypes = {};
 
 // createCard.defaultProps = {};
