66c52ea28070418d3c774b4049c4765bedfb2327
clément moretti <98534268+daddykoala@users.noreply.github.com>
2023-05-30 15:24:28 +0200

    add expect sidebar.test

--- my-app/src/--test--/sidebar.test.js
+++ my-app/src/--test--/sidebar.test.js
@@ -1,14 +1,43 @@
-import { screen} from "@testing-library/react"
+import { screen } from "@testing-library/react";
 import { renderWithProviders } from "./test-utils";
 import Sidebar from "../app/components/sidebar/sidebar";
 
+describe("sidebar", () => {
+  let preloadedState;
+  
+  preloadedState = {
+    user: {
+      user: {
+        id: "1",
+        name: "Test User",
+        table: [
+          { id: "1", name: "Table 1", list: [] },
+          { id: "2", name: "Table 2", list: [] },
+          { id: "3", name: "Table 3", list: [] },
+          { id: "4", name: "Table 4", list: [] },
+        ],
+      },
+    },
+    sidebar: {
+      isOpen: true,
+    },
+  };
+  beforeEach(() => {
+      renderWithProviders(<Sidebar />, { preloadedState });
+});
 
+  it("should render sidebar", () => {
+    expect(screen.getByText("mes tableaux")).toBeInTheDocument();
+  });
 
-describe('sidebar', () => {
-    it('should render sidebar', () => {
-        
-       renderWithProviders(<Sidebar />);
-        expect(screen.getByText('mes tableaux')).toBeInTheDocument();
+  // vérifie que les titres sont présents
+  it("should render children", () => {
+    const titles = screen.getAllByText(/Table \d/);
+    expect(titles).toHaveLength(preloadedState.user.user.table.length);
+
+    // vérifie le contenu de chaque titre
+    preloadedState.user.user.table.forEach((table, index) => {
+      expect(titles[index]).toHaveTextContent(table.name);
     });
-}
-);
+  });
+});
--- my-app/src/--test--/test-utils.tsx
+++ my-app/src/--test--/test-utils.tsx
@@ -2,11 +2,11 @@ import React from 'react'
 import { render } from '@testing-library/react'
 import { configureStore } from '@reduxjs/toolkit'
 import { Provider } from 'react-redux'
+import { MemoryRouter } from 'react-router-dom';
 // As a basic setup, import your same slice reducers
 import {authSlice} from '../app/store/reducer/authSlice'
 import {sidebarSlice} from '../app/store/reducer/sidebarSlice'
 import {userSlice} from '../app/store/reducer/userSlice'
-
 
 export function renderWithProviders(
   ui,
@@ -18,7 +18,7 @@ export function renderWithProviders(
   } = {}
 ) {
   function Wrapper({ children }) {
-    return <Provider store={store}>{children}</Provider>
+    return <Provider store={store}><MemoryRouter>{children}</MemoryRouter></Provider>
   }
 
   // Return an object with the store and all of RTL's query functions
